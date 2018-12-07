26b4503bffd367008586a40a61862de60b2467f6
Unknown <jorge.ayala@usach.cl>
2018-12-07 16:02:49 -0300

    Prueba commit Jorge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Esto es una prueba
                 </div>
             </div>
         </div>
