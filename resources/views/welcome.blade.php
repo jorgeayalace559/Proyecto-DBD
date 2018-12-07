36fbff035162253fb8751e021b3ed175e863f9ec
Unknown <jorge.ayala@usach.cl>
2018-12-07 16:49:25 -0300

    Cambie el Título del proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    GRUPO 11
+                    LATAM
                 </div>
 
                 <div class="links">
