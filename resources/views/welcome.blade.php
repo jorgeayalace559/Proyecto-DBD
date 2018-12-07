832efc474058877212847eccf0a734a2a7e6f85e
Unknown <jorge.ayala@usach.cl>
2018-12-07 16:39:13 -0300

    Titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Grupo 12
                 </div>
 
                 <div class="links">
