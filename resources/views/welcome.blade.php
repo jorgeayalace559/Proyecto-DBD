c165a0a02fb19274a01a237f1f858592e27e42cd
Nicolás López González <nicolas.lopez.g@usach.cl>
2018-12-07 16:37:01 -0300

    gitflow-feature-stash: Nicolas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GRUPO 11
                 </div>
 
                 <div class="links">
