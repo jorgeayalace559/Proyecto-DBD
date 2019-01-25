dfbb127d4ec479da2f5b0c6ab6acd0c206f86041
Nicolás López González <nicolas.lopez.g@usach.cl>
2019-01-25 06:42:27 -0300

    Mas vistas

--- resources/views/state/show.blade.php
+++ resources/views/state/show.blade.php
@@ -1,38 +1,80 @@
-<body>
-<div class="content">
-    <div class="title m-b-md">
-        Estado del Vuelo    
-    </div>
-    <br> <br>
-    <form action="/action_page.php">
-        Ingrese Número del Vuelo:
-        <input name="tripDate" type="text">
-    </form>
-    <br> <br>
-    <form action="/action_page.php">
-        ¿A donde Viajas?:
-        <input name="tripDate" type="text">
-    </form>
-    <br> <br>
-    <div style="width:400px;">
-        <div style="float: left; width: 130px"> 
-            <form action="/action_page.php">
-                Salida:
-                <br> <br>
-                <input type="date" name="fecha">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head> 
+        <title>Estado</title>
+
+            <!--  jQuery -->
+            <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+            <!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
+            <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+
+            <!-- Bootstrap Date-Picker Plugin -->
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+            <!-- Styles -->
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+            <!doctype html>
+        
+
+            <!-- Fonts -->
+            <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="/">MIATAM</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Principal <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Reserva Tu Vuelo</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Check-In</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/state">Estado del Vuelo</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Vuelo, Hotel y Auto</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </nav>
+    </head>
+    <body>
+        <div class="container">
+            <form>
+              <div class="form-group">
+                <label for="numeroVuelo">Vuelo</label>
+                <input type="number" class="form-control" id="numeroVuelo" aria-describedby="emailHelp" placeholder="Ejemplo: 1515">
+                <small id="emailHelp" class="form-text text-muted">El numero lo puedes encontrar en el boleto.</small>
+              </div>
+              <div class="form-group">
+                <label for="numeroVuelo">Fecha</label>
+                    <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
+                 </div>
+              <button type="submit" class="btn btn-primary">Ver estado del vuelo</button>
             </form>
-        <br> <br>
-        </div>
-            <div style="float: right; width: 225px"> 
-            <form action="/action_page.php">
-                Llegada:
-                <br> <br>
-                <input type="date" name="fecha">
-            </form>
-        </div>
-        <br><br>
-        <form action="/action_page.php">
-            <input type="submit" value="Ver Estado">
-        <form>
+                <script>
+                    $(document).ready(function(){
+                      var date_input=$('input[name="date"]'); //our date input has the name "date"
+                      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+                      var options={
+                        format: 'mm/dd/yyyy',
+                        container: container,
+                        todayHighlight: true,
+                        autoclose: true,
+                      };
+                      date_input.datepicker(options);
+                    })
+                </script>
     </div>
-<body>
+</div>
+    </body>
+</html>
