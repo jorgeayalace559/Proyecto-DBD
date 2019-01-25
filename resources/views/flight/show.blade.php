0fd0c32a036865d63e3e0ced4b3f446e0f541596
Javier Pérez <javier.perez.ca@usach.cl>
2019-01-25 04:58:27 -0300

    Factorys Arreglados

--- resources/views/flight/show.blade.php
+++ resources/views/flight/show.blade.php
@@ -4,52 +4,11 @@ Vuelos Disponibles
 <br><br>
 <br><br>
 
-<html>
-    <head> 
-        <title>Menu Desplegable</title> 
-            <style type="text/css"> 
-
-                * { 
-                    margin:0px; padding:0px; 
-                } 
-                #header { 
-                    margin:auto; width:175px; font-family:Arial, Helvetica, sans-serif;
-                }
-                ul, ol { 
-                    list-style:none; 
-                }
-                .nav li a { 
-                    background-color:#245; color:#fff; text-decoration:none; padding:10px 12px; display:block;
-                    position:relative; right:550px;
-                }
-                .nav li a:hover { background-color:#255; } .nav li ul { display:none; position:absolute; min-width:140px; }
-                
-            </style> 
-            </head>
-                <body> 
-                    <div id="header"> 
-                        <ul class="nav"> 
-                            <a href="">Reserva Tu Vuelo</a>
-                            
-                             
-                            <li><a href="">Check-In</a>
-                            
-                            
-                            </li> 
-                            <li><a href="">Estado del Vuelo</a>
-                            
-                            
-                            </li> 
-                            <li><a href="">Vuelo, Hotel y Auto</a>
-                            
-                            
-                            </li> 
-                        </ul>
-                    </div> 
-                </body> 
-            </html>
 
 
+
+
+<!--
 <p>Display some text when the checkbox is checked:</p>
 Checkbox: <input type="checkbox" id="myCheck"  onclick="myFunction()">
 <p id="text" style="display:none">Checkbox is CHECKED!</p>
@@ -64,3 +23,4 @@ function myFunction() {
   }
 }
 </script>
+-->
