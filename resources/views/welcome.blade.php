7aa218be0af952522774b04a0a3b939765c93d79
Javier Pérez <javier.perez.ca@usach.cl>
2019-01-25 07:31:15 -0300

    Problema Vista Principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,79 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head> 
+        <title>MIATAM</title> 
 
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!doctype html>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">MIATAM</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Principal <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reserva Tu Vuelo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Check-In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Estado del Vuelo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Vuelo, Hotel y Auto</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </head>
+    <body>  
+        <br><br>
+        <div class="col-sm-11">
+            <div class="row">
+                <div class="col-3">
+                    <div style="width:200px;">
+                        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+                            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+                            <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+                            <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-9">
+                    <div class="tab-content" id="v-pills-tabContent">
+                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">a</div>
+                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">b</div>
+                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">c</div>
+                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">d</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+<!--
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>MIATAM</title>
-
-        <!-- Styles -->
+        
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!doctype html>
         
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
+        
         <style>
             html, body {
                 background-color: #fff;
@@ -145,10 +204,7 @@
         </div>
     </body>
 </html>
-
-
-
 </body>
-
-<!-- Scripts -->
+    Scripts
 <script src="{{ asset('js/app.js') }}"></script>
+-->
